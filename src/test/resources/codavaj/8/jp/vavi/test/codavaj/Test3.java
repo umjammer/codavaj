82cbeb6f84aa8070db7282a528c386dc701d60f1
umjammer <umjammer@gmail.com>
2019-05-09 12:25:24 +0900

    add unit test

---
+++ src/test/resources/codavaj/8/jp/vavi/test/codavaj/Test3.java
@@ -0,0 +1,21 @@
+package vavi.test.codavaj;
+/**
+ * description of this enum.
+ * more description of this enum.
+ * @version 0.00 2019/05/07 umjammer initial version
+ * @author <A href="mailto:umjammer@gmail.com">Naohide Sano</A> (umjammer)
+ */
+public enum Test3{
+    /**
+     * description for this field.
+     * more description.
+     */
+    Member1,
+
+    /**
+     * description for this field.
+     * more description.
+     */
+    Member2;
+
+}
